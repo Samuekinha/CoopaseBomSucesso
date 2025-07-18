06ede11550524e506e5ff3aa0ddd7113a140bd41
SamuelDC <samuelcomunello939@gmail.com>
2025-07-18 16:52:45 -0300

    AJUSTES REQUERIDOS

--- src/test/java/com/example/moinho/MoinhoApplicationTests.java
+++ src/test/java/com/example/moinho/MoinhoApplicationTests.java
@@ -2,12 +2,13 @@ package com.example.moinho;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // Isso fará com que use as configurações de teste
 class MoinhoApplicationTests {
-
 	@Test
 	void contextLoads() {
+		// Teste vazio apenas para verificar se o contexto carrega
 	}
-
 }
